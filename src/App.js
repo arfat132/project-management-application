30d19cba13cd1c6a7a072bf01558e617baf89748
arfat132 <arfatbegum90@gmail.com>
2022-09-20 22:43:10 +0300

    login persistancy implemented

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import Login from './pages/Login'
 import { Routes, Route } from "react-router-dom";
 import Teams from './pages/Teams';
 import Projects from './pages/Projects';
+import useAuthCheck from "./hooks/useAuthCheck";
 function App() {
-  return (
+
+  const authChecked = useAuthCheck();
+
+  return !authChecked ? (
+    <div>Checking authentication....</div>
+  ) : (
     <div className="App">
-       <Routes>
+      <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/teams" element={<Teams />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </div>
   );
+  
 }
 
 export default App;
